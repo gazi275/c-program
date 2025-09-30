3e8ace42d93f1dcb730b3c0e0a0aba70ed6cda3c
gazi275 <gaziazad270@gmail.com>
2025-09-30 21:16:28 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ String/lenWithOutstrlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char A[100];
+    scanf("%s", A);
+    int count = 0;
+    for (int i = 0; A[i] != '\0'; i++)
+    {
+        count++;
+    }
+    printf("%d", count);
+    return 0;
+}
