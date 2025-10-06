ae7a27ef16f4cea2c22c088ec036b267a16920d9
gazi275 <gaziazad270@gmail.com>
2025-10-07 01:10:54 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ String/compareTwoString.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char A[1001];
+    scanf("%s", A);
+    char B[1001];
+    scanf("%s", B);
+    int flag = 0;
+    for (int i = 0; A[i] != '\0' || B[i] != '\0'; i++)
+    {
+        if (A[i] != B[i])
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
+}
