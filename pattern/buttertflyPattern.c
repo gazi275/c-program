75d23d7c98dc5afe3d3d7a8701b4920e7232bb95
gazi275 <gaziazad270@gmail.com>
2025-10-13 23:59:01 +0600

    Fix butterfly pattern logic for lower part and update binary

--- pattern/buttertflyPattern.c
+++ pattern/buttertflyPattern.c
@@ -26,7 +26,7 @@ int main() {
     }
 
     // Lower part
-    for (int i = n; i >= 1; i--) {
+    for (int i = n-1; i >= 1; i--) {
         // Left stars
         for (int j = 1; j <= i; j++) {
             printf("*");
