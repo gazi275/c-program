106ce6d6f8ae48d31778b427a27edb23496e712f
gazi275 <gaziazad270@gmail.com>
2025-10-16 20:21:50 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ Array/sorting.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int A[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+    for(int i=0; i<n-1;i++){
+        for(int j=i+1; j<n;j++){
+            if(A[i]>A[j]){
+                int temp =A[i];
+                A[i]=A[j];
+                A[j]=temp;
+            }
+        }
+    }
+   for (int i = 0; i < n; i++)
+   {
+       printf("%d ", A[i]);
+   }
+   printf("\n");
+   return 0;
+}
